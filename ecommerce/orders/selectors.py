f1235f0e0244d2f4469ac2bd498946806903cbf2
juanpsenn <juanpsenn@gmail.com>
2022-12-27 12:07:34 +0000

    Annotate

--- ecommerce/orders/selectors.py
+++ ecommerce/orders/selectors.py
@@ -1,6 +1,5 @@
-from articles.models import Article, Author
-from django.db.models import Q
-from articles.filters import title_or_description_contains, author_name_contains
+from orders.models import OrderDetail
+from django.db.models import Q, F
 
 # orders = OrderDetail.objects.filter(article_id__in=[1, 7]).values_list("order", flat=True)
 # orders = Order.objects.filter(id__in=orders)
@@ -10,3 +9,8 @@ from articles.filters import title_or_description_contains, author_name_contains
 # for detail in details:
 #     if not detail.order in orders:
 #         orders.append(detail.order)
+
+def get_order_details(order):
+    return OrderDetail.objects.filter(order_id=order).annotate(
+        current_price=F("article__price")
+    )
